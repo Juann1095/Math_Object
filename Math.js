c2b851c514e95ac6271f4dfeddf1e51f6062616f
Juan Romero <slayerforce880@gmail.com>
2023-03-29 14:00:51 -0600

    Exercise Upload: Math Objects
    Exercise working with JavaScript built-in Math Objects.

---
+++ Math.js
@@ -0,0 +1,41 @@
+//JavaScript has handy built-in objects. One of these popular built-in objects is the Math object.
+
+//------Number Constants------
+
+//PI
+console.log(Math.PI) 
+//Euler´s Constant
+console.log(Math.E)
+//Natural Logarithm of 2
+console.log(Math.LN2)
+
+//------Arithmetic and Calculus methods------
+//ceil Rounds up to the closest integer   --8
+console.log(Math.ceil(7.8))
+//floor rounds down to the closest integer --7
+console.log(Math.floor(7.8))
+//round rounds up to the closest integer if the decimal is .5 or above; otherwise, rounds down to closest integer -- 8
+console.log(Math.round(7.8))
+//trims the decimal, leaving only the integer --7
+console.log(Math.trunc(7.8))
+
+//------Arithmetic and Calculus methods------
+//pow calculates the number 2 to the power of 3, the result is 8 
+console.log(Math.pow(2,3))
+//sqrt calculates the square root of 16, the result is 4
+console.log(Math.sqrt(16))
+// cbrt finds the cube root of 8, the result is 2
+console.log(Math.cbrt(8))
+// abs returns the absolute value, the result is 10
+console.log(Math.abs(-10))
+// log Logarithmic methods
+console.log(Math.log(10))
+console.log(Math.log2(10))
+console.log(Math.log10(10))
+//min,max return the minimum and maximum values of all the inputs:
+console.log(Math.min(9,8,7))
+console.log(Math.max(9,8,7))
+//Trigonometric methods sin, cos , tan
+console.log(Math.sin(50))
+console.log(Math.cos(50))
+console.log(Math.tan(50))
